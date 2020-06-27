70b7eaae94714a0df8509c926271063e6bb01928
soichi <soi.watanabe.chi@gmail.com>
2020-06-27 20:58:02 +0900

    fix: bug in calc_fizzbuzz.go case 7 #2

--- fizzbuzzFunc/calc_fuzzbuzz.go
+++ fizzbuzzFunc/calc_fuzzbuzz.go
@@ -6,14 +6,14 @@ import (
 )
 
 func FizzBuzz(i int) string {
-	if i%15 == 0 {
+	if strings.Contains(strconv.Itoa(i), "7") {
+		return "GitHub-fixed"
+	} else if i%15 == 0 {
 		return "FizzBuzz"
 	} else if i%3 == 0 {
 		return "Fizz"
 	} else if i%5 == 0 {
 		return "Buzz"
-	} else if strings.Contains(strconv.Itoa(i), "7") {
-		return "GitHub-fixed"
 	} else {
 		return strconv.Itoa(i)
 	}
