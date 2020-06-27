fdc04f12c8e55acba99793f3db48353117b3d594
soichi <soi.watanabe.chi@gmail.com>
2020-06-27 20:43:38 +0900

    refactor:make calc_fizzbuzz.go

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+
+	"./fizzbuzzFunc"
 )
 
 func main() {
 	for i := 0; i < 101; i++ {
-		fmt.Println(FizzBuzz(i))
+		fmt.Println(fizzbuzzFunc.FizzBuzz(i))
 	}
 
-}
-
-func FizzBuzz(i int) string {
-	if i%15 == 0 {
-		return "FizzBuzz"
-	} else if i%3 == 0 {
-		return "Fizz"
-	} else if i%5 == 0 {
-		return "Buzz"
-	} else if strings.Contains(strconv.Itoa(i), "7") {
-		return "GitHub-fixed"
-	} else {
-		return strconv.Itoa(i)
-	}
 }
