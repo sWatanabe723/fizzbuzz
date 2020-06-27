a5e4d7a349e1dee3dc43f4ccfb55588bf4989f8f
soichi <soi.watanabe.chi@gmail.com>
2020-06-27 20:10:03 +0900

    refactor: make func FizzZBuzz #2

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -8,17 +8,21 @@ import (
 
 func main() {
 	for i := 0; i < 101; i++ {
-		if i%15 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else if strings.Contains(strconv.Itoa(i), "7") {
-			fmt.Println("GitHub-fixed")
-		} else {
-			fmt.Println(i)
-		}
+		FizzBuzz(i)
 	}
 
+}
+
+func FizzBuzz(i int) {
+	if i%15 == 0 {
+		fmt.Println("FizzBuzz")
+	} else if i%3 == 0 {
+		fmt.Println("Fizz")
+	} else if i%5 == 0 {
+		fmt.Println("Buzz")
+	} else if strings.Contains(strconv.Itoa(i), "7") {
+		fmt.Println("GitHub-fixed")
+	} else {
+		fmt.Println(i)
+	}
 }
